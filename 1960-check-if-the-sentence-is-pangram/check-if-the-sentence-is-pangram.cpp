214efc1d954c13d90c8478427fd37e43365324da
Qasim Ahmad <160157239+Qasimahmad55@users.noreply.github.com>
2026-01-30 21:50:39 +0500

    Time: 1 ms (16.97%) | Memory: 9.1 MB (6.07%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool checkIfPangram(string sentence) {
         unordered_set<char> letters(sentence.begin(), sentence.end());
-        return letters.size() == 26;
+        
+        return count_if(letters.begin(), letters.end(), 
+                       [](char c) { return c >= 'a' && c <= 'z'; }) == 26;
     }
 };
