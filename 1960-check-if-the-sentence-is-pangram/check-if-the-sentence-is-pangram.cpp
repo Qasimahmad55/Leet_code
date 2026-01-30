b7615101fc020c003300d647dad8fd5681437d2d
Qasim Ahmad <160157239+Qasimahmad55@users.noreply.github.com>
2026-01-30 21:46:27 +0500

    Time: 1 ms (16.97%) | Memory: 9.2 MB (6.07%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_set<char> letters(sentence.begin(), sentence.end());
+        return letters.size() == 26;
+    }
+};
