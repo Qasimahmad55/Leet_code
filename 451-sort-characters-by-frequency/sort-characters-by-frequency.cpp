f85d5fbe6b09a0be018c3c5a057efa779b1cea70
Qasim Ahmad <160157239+Qasimahmad55@users.noreply.github.com>
2026-02-01 15:20:28 +0500

    Time: 8 ms (12.73%) | Memory: 17.2 MB (6.60%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> freq;
+        for (char c : s) {
+            freq[c]++;
+        }
+        vector<vector<char>> buckets(s.length() + 2);
+        for (auto& [ch, count] : freq) {
+            buckets[count].push_back(ch);
+        }
+        string result;
+        for (int i = buckets.size() - 1; i >= 0; i--) {
+            for (char ch : buckets[i]) {
+                result.append(i, ch); 
+            }
+        }
+        
+        return result;
+    }
+};
