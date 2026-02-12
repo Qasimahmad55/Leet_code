fff5cf633ebe073618eec105dc78a4f0e23aee91
Qasim Ahmad <160157239+Qasimahmad55@users.noreply.github.com>
2026-02-12 16:17:58 +0500

    Time: 0 ms (100.00%) | Memory: 9.6 MB (96.48%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,45 +1,35 @@
 class MyQueue {
 private:
-    stack<int> s1;  // Main stack
-    stack<int> s2;  // Helper stack
+    stack<int> input;  
+    stack<int> output;  
     
 public:
     MyQueue() {
         
     }
     
-    // O(n) - transfer all elements
     void push(int x) {
-        // Move all elements from s1 to s2
-        while (!s1.empty()) {
-            s2.push(s1.top());
-            s1.pop();
-        }
-        
-        // Push new element to s1
-        s1.push(x);
-        
-        // Move everything back to s1
-        while (!s2.empty()) {
-            s1.push(s2.top());
-            s2.pop();
-        }
+        input.push(x);
     }
     
-    // O(1)
     int pop() {
-        int front = s1.top();
-        s1.pop();
+        peek(); 
+        int front = output.top();
+        output.pop();
         return front;
     }
     
-    // O(1)
     int peek() {
-        return s1.top();
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
     }
     
-    // O(1)
     bool empty() {
-        return s1.empty();
+        return input.empty() && output.empty();
     }
 };
