c38ef717bf700d3a9536ad2b411b31aee7207ba2
Qasim Ahmad <160157239+Qasimahmad55@users.noreply.github.com>
2026-02-12 22:44:43 +0500

    Time: 0 ms (100.00%) | Memory: 9.4 MB (62.77%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,54 +1,33 @@
 class MyStack {
 private:
-    queue<int> q1;  // Main queue
-    queue<int> q2;  // Helper queue
+    queue<int> q;
     
 public:
     MyStack() {
         
     }
     
-    // O(1)
     void push(int x) {
-        q1.push(x);
+        int size = q.size();
+        q.push(x);
+        
+        for (int i = 0; i < size; i++) {
+            q.push(q.front());
+            q.pop();
+        }
     }
     
-    // O(n) - move n-1 elements to q2
     int pop() {
-        // Move all except last element to q2
-        while (q1.size() >1) {
-            q2.push(q1.front());
-            q1.pop();
-        }
-        
-        // Get the last element (top of stack)
-        int top = q1.front();
-        q1.pop();
-        
-        // Swap q1 and q2
-        swap(q1, q2);
-        
+        int top = q.front();
+        q.pop();
         return top;
     }
     
-    // O(n)
     int top() {
-        while (q1.size() >1) {
-            q2.push(q1.front());
-            q1.pop();
-        }
-        
-        int topElement = q1.front();
-        q2.push(q1.front());  // Don't forget to move it
-        q1.pop();
-        
-        swap(q1, q2);
-        
-        return topElement;
+        return q.front();
     }
     
-    // O(1)
     bool empty() {
-        return q1.empty();
+        return q.empty();
     }
 };
