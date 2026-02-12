3c27820bbf035a6564e241c0eba7a0f6778fe8cc
Qasim Ahmad <160157239+Qasimahmad55@users.noreply.github.com>
2026-02-12 12:51:51 +0500

    Time: 11 ms (42.68%) | Memory: 14.7 MB (26.69%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        unordered_set<int> toRemove;
-        stack<int> st;
-        
-        // First pass: Find indices of invalid parentheses
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(') {
-                st.push(i);
-            } else if (s[i] == ')') {
-                if (!st.empty()) {
-                    st.pop();  // Matched
-                } else {
-                    toRemove.insert(i);  // Invalid ')'
-                }
+        int balance = 0;
+        string temp = "";
+
+        for (char c : s) {
+            if (c == '(') {
+                balance++;
+            } else if (c == ')') {
+                if (balance == 0)
+                    continue;
+                balance--;
             }
+            temp += c;
         }
-        
-        // Remaining in stack are unmatched '('
-        while (!st.empty()) {
-            toRemove.insert(st.top());
-            st.pop();
-        }
-        
-        // Build result excluding invalid indices
+
         string result = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (toRemove.find(i) == toRemove.end()) {
-                result += s[i];
+        balance = 0;
+
+        for (int i = temp.length() - 1; i >= 0; i--) {
+            if (temp[i] == ')') {
+                balance++;
+            } else if (temp[i] == '(') {
+                if (balance == 0)
+                    continue;
+                balance--;
             }
+            result += temp[i];
         }
-        
+
+        reverse(result.begin(), result.end());
         return result;
     }
 };
