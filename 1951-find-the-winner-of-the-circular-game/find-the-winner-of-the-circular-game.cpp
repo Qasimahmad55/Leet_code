278fbda01dfee09a21ffa0c1458fdeb0c19c1681
Qasim Ahmad <160157239+Qasimahmad55@users.noreply.github.com>
2026-02-14 20:35:25 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (79.69%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        queue<int> q;
+        int winner = 0;  
         
-        for (int i = 1; i <= n; i++) {
-            q.push(i);
+        for (int i = 2; i <= n; i++) {
+            winner = (winner + k) % i;
         }
         
-        while (q.size() >1) {
-            for (int i = 0; i < k - 1; i++) {
-                q.push(q.front());
-                q.pop();
-            }
-            
-            q.pop();
-        }
-        
-        return q.front();
+        return winner + 1; 
     }
 };
