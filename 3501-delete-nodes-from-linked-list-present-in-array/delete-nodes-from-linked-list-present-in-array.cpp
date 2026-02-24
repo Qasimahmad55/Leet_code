9cbdc327e615d355531f341fbb634d3e849b12ed
Qasim Ahmad <160157239+Qasimahmad55@users.noreply.github.com>
2026-02-24 22:33:41 +0500

    Time: 100 ms (46.00%) | Memory: 263.4 MB (54.24%) - LeetSync

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* curr = &dummy;
+        
+        while (curr->next) {
+            if (s.count(curr->next->val)) {
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
+            }
+        }
+        return dummy.next;
+    }
+};
